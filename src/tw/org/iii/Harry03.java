0bd842f554422a0af5d1cea58cb7ebdf21f2a7b9
harry930216 <cl4m4peng@yahoo.com.tw>
2026-04-29 15:01:48 +0800

    0429 03:01

---
+++ src/tw/org/iii/Harry03.java
@@ -0,0 +1,21 @@
+package tw.org.iii;
+
+public class Harry03 {
+
+	public static void main(String[] args) {
+		// float, double
+		float var1 = 12;
+		float var2 = 12.3f;
+		double var3 = 12.3;
+		
+		// char 
+		char c1 = 'a';
+		char c2 = 65; // 2^16 字元沒有負數
+		char c3 = '資';
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+
+	}
+
+}
