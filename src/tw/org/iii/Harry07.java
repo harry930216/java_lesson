30fdd45238eb03259cd89279effd0d43aff81f9f
harry930216 <cl4m4peng@yahoo.com.tw>
2026-04-30 14:03:47 +0800

    07修正

--- src/tw/org/iii/Harry07.java
+++ src/tw/org/iii/Harry07.java
@@ -2,22 +2,41 @@ package tw.org.iii;
 
 public class Harry07 {
 
-	int i = 0;
+	static int i = 1;
+
 	public static void main(String[] args) {
-		
-				
+
 		for (initial(); i < 10; contin()) {
-			System.out.printf("步驟2第 + %d + 次",i+1);
+			System.out.printf("步驟2第%d次", i);
+			System.out.println();
 		}
 
 	}
-	
+
 	public static void initial() {
 		System.out.println("初始值必定執行-步驟1");
 	}
-	
+
 	public static void contin() {
-		i++;
-		System.out.printf("步驟3第 + %d + 次",i+1);
+		System.out.printf("步驟3第%d次", i++);
+		System.out.println();
+		
+		
 	}
+	
+//	public static void main(String[] args) {
+//		int i = 0;
+//		for (printBrad(); i < 10; printLine()) {
+//			System.out.println(i++);
+//		}
+//
+//	}
+//	
+//	static void printBrad() {
+//		System.out.println("Brad");
+//	}
+//	
+//	static void printLine() {
+//		System.out.println("-------");
+//	}
 }
