1f48b3f27dd495a40f94a40dcf05643834135206
harry930216 <cl4m4peng@yahoo.com.tw>
2026-05-07 17:12:12 +0800

    Merge branch 'master' of https://github.com/harry930216/java_lesson

--- src/tw/org/iii/GuessNumberSanbox.java
+++ src/tw/org/iii/GuessNumberSanbox.java
@@ -1,18 +1,37 @@
 package tw.org.iii;
 
-import javax.swing.JFrame;
+import java.util.ArrayList;
+import java.util.List;
 
-public class GuessNumberSanbox extends JFrame {
+class Customer {
+    String name;
+    List<Order> orders = new ArrayList<>();
+    
+    void addOrder(Order o) {
+        orders.add(o);
+    }
+}
 
-	public static void main(String[] args) {
-		JFrame guessNumber = new JFrame("猜數字");
-		
-		guessNumber.setVisible(true);
-		guessNumber.setSize(600,400);
-		guessNumber.setDefaultCloseOperation(0);
-
-	}
-	
-//	public GuessNumber()
-
+class Order {
+    String item;
+    Customer customer;
+    
+    void assignTo(Customer c) {
+        this.customer = c;          // ① 我記住客人
+        c.addOrder(this);           // ② 讓客人也記住「我」
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Customer alice = new Customer();
+        alice.name = "Alice";
+        
+        Order o1 = new Order();
+        o1.item = "Book";
+        
+        o1.assignTo(alice);
+        
+        System.out.println(o1.customer.name);          // Alice
+        System.out.println(alice.orders.get(0).item);  // Book
+    }
 }
